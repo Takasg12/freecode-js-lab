fea1d2df6a9b897c0d81b0bfb441cee178ce76a0
Takasg12 <rabbaniraya465@gmail.com>
2025-12-15 19:32:10 +0700

    Build a JavaScript Trivia Bot

---
+++ variables and strings/build a javascript trivia bot/script.js
@@ -0,0 +1,25 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+/* membuat variabel */
+let botName = "CodeBot";
+let botLocation = "JavaScript Island";
+let favoriteLanguage = "JavaScript";
+
+/* Memanggil variable botName dan botLocation */
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+/* Memanggil variable favoriteLanguage */
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+/* membuat codingFact variable */
+let codingFact = favoriteLanguage + " was created in just 10 days by brendan eich in 1995.";
+console.log(codingFact);
+
+/* membuat variable codingFact dengan beda string */
+codingFact = "Despite the name, " + favoriteLanguage + "has nothing to do with Java.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is one of the most populer programming languages in the world, used by cover 97% of websitets.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + "from " + botLocation + ".");
