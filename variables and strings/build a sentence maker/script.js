cafac35f67fa48f3a41798d0ab483b8a626c48a0
Takasg12 <rabbaniraya465@gmail.com>
2025-12-16 18:33:40 +0700

    Build a Sentence Maker

---
+++ variables and strings/build a sentence maker/script.js
@@ -0,0 +1,41 @@
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+
+/* Menetapkan nilai-nilai untuk variabel-variabel tersebut */
+adjective = "spaekly";
+noun = "dragon";
+verb = "sleeping";
+place = "candy castle";
+adjective2 = "flaming";
+noun2 = "chooclate bars";
+
+/* Membuat variable baru */
+let firstStory;
+
+/* Mengisi variable firstStory */
+firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+/* Menampilkan cerita pertama atau firstStory */
+console.log("First story: " + firstStory);
+
+
+/* Membuat nilai baru untuk variabel yang sama */
+adjective = "gigantic";
+noun = "robot";
+verb = "dancing";
+place = "underwater city";
+adjective2 = "shiny";
+noun2 = "pizza";
+
+/* Membuat variable ke 2 untuk cerita kedua */
+let secondStory;
+
+/* Mengisi variable secondStory */
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+/* Menampilkan cerita kedua atau secondStory */
+console.log("Second story: " + secondStory);
