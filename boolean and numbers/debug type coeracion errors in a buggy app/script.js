055004192f603d2574a5be4215f1df8352e6673f
Takasg12 <rabbaniraya465@gmail.com>
2025-12-27 22:39:58 +0700

    Debug Type Coeracion Errors In a Buggy App

---
+++ boolean and numbers/debug type coeracion errors in a buggy app/script.js
@@ -0,0 +1,17 @@
+const firstResult = 5 + 10;
+console.log("5 + 10 = " + firstResult)
+
+const secondResult = 8 - 5;
+console.log("8 - 5 = " + secondResult);
+
+const thirdResult = 2 + 4;
+console.log("2 + 4 = " + thirdResult);
+
+const fourthResult = 3 + 5;
+console.log("3 + 5 = " + fourthResult);
+
+const fifthResult = 4 * 5;
+console.log("4 * 5 = " + fifthResult)
+
+const sixthResult = 10 + 12;
+console.log("10 + 12 = " + sixthResult)
