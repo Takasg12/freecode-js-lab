9c309e25aaa9f8884880ddbe98b1f97e5e080356
Takasg12 <rabbaniraya465@gmail.com>
2025-12-30 22:26:11 +0700

    Debug Increment and Decrement Operator Errors In a Buggy App

---
+++ boolean and numbers/debug increment and decrement operator errors in a buggy app/script.js
@@ -0,0 +1,15 @@
+let counter = 10;
+let updatedCounter = ++counter;
+console.log(updatedCounter)
+
+let score = 8;
+let finalScore = score++;
+console.log(finalScore)
+
+let coins = 3;
+let updatedCoins = --coins;
+console.log(coins)
+
+let health = 7;
+let newHealth = health--;
+console.log(health)
