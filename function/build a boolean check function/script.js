01b4d4d66597b7880c078fe3088d125b41c28b97
Takasg12 <rabbaniraya465@gmail.com>
2026-01-05 22:59:48 +0700

    Build a Boolean Check Function

---
+++ function/build a boolean check function/script.js
@@ -0,0 +1,14 @@
+const booWho = (bool) => {
+    return typeof bool === "boolean";
+}
+
+console.log(booWho(true)); // true
+console.log(booWho(false)); //true
+console.log(booWho([1, 2, 3,])); //false
+console.log(booWho([].slice)); //false
+console.log(booWho({ "a": 1 })); //false
+console.log(booWho(1)); //false
+console.log(booWho(NaN)); //false
+console.log(booWho("a")); //false
+console.log(booWho("true")); //false
+console.log(booWho("false")); //false
