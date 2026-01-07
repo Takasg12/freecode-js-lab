07c0e504b2b03103779ea07dc2a46856c0f77d0c
Takasg12 <rabbaniraya465@gmail.com>
2026-01-07 21:53:47 +0700

    Build a Celsius To Fahrenheit Converter

---
+++ function/build a celsius to fahrenheit converter/script.js
@@ -0,0 +1,7 @@
+const convertCtoF = (celsius) => {
+    return (celsius * 9 / 5) + 32;
+}
+
+console.log(convertCtoF(0));    // 32
+console.log(convertCtoF(100));  // 212
+console.log(convertCtoF(20));   // 68
