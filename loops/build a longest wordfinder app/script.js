7596f0e786b72c6c5382d5db4bd43ee1f7f18344
Takasg12 <rabbaniraya465@gmail.com>
2026-01-29 23:56:37 +0700

    Build a Longest Wordfinder App

---
+++ loops/build a longest wordfinder app/script.js
@@ -0,0 +1,5 @@
+const findLongestWordLength = (str) => {
+    return Math.max(...str.split(' ').map(word => word.length))
+}
+
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dong"))
