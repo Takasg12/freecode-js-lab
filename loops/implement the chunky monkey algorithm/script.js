155eb963c7416cee12faa0cc4a3e7b3cfc595df5
Takasg12 <rabbaniraya465@gmail.com>
2026-02-04 21:08:00 +0700

    Implement the Chunky Monkey

---
+++ loops/implement the chunky monkey algorithm/script.js
@@ -0,0 +1,11 @@
+const chunkArrayInGroups = (arr, size) => {
+    let result = [];
+
+    for (let i = 0; i < arr.length; i += size) {
+        let chunk = arr.slice(i, i + size);
+        result.push(chunk);
+    }
+    return result;
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2)); // [["a", "b"], ["c", "d"]]
