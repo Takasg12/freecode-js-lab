f5dcad939e7c337d6b557ee358a0d038f2b225bf
Takasg12 <rabbaniraya465@gmail.com>
2026-02-11 21:32:48 +0700

    Build a String Repeating Function

---
+++ loops/build a string repeating function/script.js
@@ -0,0 +1,8 @@
+const repeatStringNumTimes = (str, num) => {
+    let result = "";
+    for (let i = 0; i < num; i++) {
+        result += str;
+    } return result;
+}
+
+console.log(repeatStringNumTimes("abc", 3)); // "abcabcabc"
