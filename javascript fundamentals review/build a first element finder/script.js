9467bbb7f7be2a50031f8ea865d8b9e0dfb3fb36
Takasg12 <rabbaniraya465@gmail.com>
2026-02-18 20:56:28 +0700

    Build a First Element Finder

---
+++ javascript fundamentals review/build a first element finder/script.js
@@ -0,0 +1,10 @@
+const findElement = (arr, func) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i]) === true) {
+            return arr[i];
+        }
+    }
+    return undefined;
+}
+
+console.log(findElement([1, 3, 5, 8, 9, 10], function (num) { return num % 2 === 0 }))
